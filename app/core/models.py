d798b7bd900dce16506b2da5f069e1de8dc424b4
tudor <tudibursuc@gmail.com>
2021-02-03 14:59:00 +0200

    Test for Superuser

--- app/core/models.py
+++ app/core/models.py
@@ -5,8 +5,17 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("Users must have an email")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
         return user
 
